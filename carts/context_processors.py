9a51205972af5f0d07abca8a27c70988cac0c645
Rathan Kumar <rathan.kumar492@gmail.com>
2021-01-11 01:03:04 +0530

    handled tax object does not exist exception

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -30,15 +30,20 @@ def shopcart_context(request):
     total=0
     tax = 0
     grand_total = 0
+    tax_percent = 0
     for i in shopcart:
         total += i.variant.price * i.quantity
 
     url = request.build_absolute_uri()
     domain = urlparse(url).netloc
     biz_id = Business.objects.get(domain_name=domain)
-    get_tax = Tax.objects.get(business__business_id=biz_id.business_id)
-    tax_percent = get_tax.tax_percentage
-    tax = round((tax_percent * total)/100, 2)
+
+    try:
+        get_tax = Tax.objects.get(business__business_id=biz_id.business_id)
+        tax_percent = get_tax.tax_percentage
+        tax = round((tax_percent * total)/100, 2)
+    except Tax.DoesNotExist:
+        tax = 0
     grand_total = round(total + tax, 2)
     test_context = 'This is test'
     return dict(shopcart=shopcart, category=category, total=total, tax_percent=tax_percent, tax=tax, grand_total=grand_total, test_context=test_context)
--- carts/views.py
+++ carts/views.py
@@ -230,15 +230,19 @@ def shopcart(request):
     total=0
     tax = 0
     grand_total = 0
+    tax_percent = 0
     for i in shopcart:
         total += i.variant.price * i.quantity
 
     url = request.build_absolute_uri()
     domain = urlparse(url).netloc
     biz_id = Business.objects.get(domain_name=domain)
-    get_tax = Tax.objects.get(business__business_id=biz_id.business_id)
-    tax_percent = get_tax.tax_percentage
-    tax = round((tax_percent * total)/100, 2)
+    try:
+        get_tax = Tax.objects.get(business__business_id=biz_id.business_id)
+        tax_percent = get_tax.tax_percentage
+        tax = round((tax_percent * total)/100, 2)
+    except Tax.DoesNotExist:
+        tax = 0
     grand_total = round(total + tax, 2)
     context={'shopcart': shopcart,
              'category':category,
