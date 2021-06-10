4670aa44dfce02b08cc41a241cbd267a8b7ea071
Rathan Kumar <rathan.kumar492@gmail.com>
2021-06-10 16:24:44 +0530

    limited captcha generation to 4 digit

--- fashion_main/static/js/ajax-js.js
+++ fashion_main/static/js/ajax-js.js
@@ -77,14 +77,11 @@ $(document).on('submit', '#newsletterForm', function(e) {
 //Created / Generates the captcha function
 function DrawCaptcha()
 {
-   var a = Math.ceil(Math.random() * 10)+ '';
-   var b = Math.ceil(Math.random() * 10)+ '';
-   var c = Math.ceil(Math.random() * 10)+ '';
-   var d = Math.ceil(Math.random() * 10)+ '';
-   var e = Math.ceil(Math.random() * 10)+ '';
-   var f = Math.ceil(Math.random() * 10)+ '';
-   var g = Math.ceil(Math.random() * 10)+ '';
-   var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' '+ f + ' ' + g;
+   var a = Math.ceil(Math.random() * 9)+ '';
+   var b = Math.ceil(Math.random() * 9)+ '';
+   var c = Math.ceil(Math.random() * 9)+ '';
+   var d = Math.ceil(Math.random() * 9)+ '';
+   var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d;
    document.getElementById("txtCaptcha").value = code
 }
 
