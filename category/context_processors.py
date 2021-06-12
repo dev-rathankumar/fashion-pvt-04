cb3622544ce13c814a53c0a8c4221ecae9b9a595
Rathan Kumar <rathan.kumar492@gmail.com>
2021-06-12 16:55:18 +0530

    footer quickshop links with first 10 parent categories

--- category/context_processors.py
+++ category/context_processors.py
@@ -2,4 +2,5 @@ from .models import Category
 
 def category_names(request):
     category = Category.objects.all()
-    return dict(category=category)
+    quickshopCat = Category.objects.filter(parent=None).order_by('id')[:10]
+    return dict(category=category, quickshopCat=quickshopCat)
